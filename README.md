048b09deabacdce89f3f4e778af06dcb0c297ac4
Ruthalas <ruthalas.bounce@gmail.com>
2020-05-24 23:12:31 -0700

    Roughed out config file

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
  Scraper for the '(not quite daily) Daily Comic'
 
 Currently under development
-A project to learn selenium
+
+Dependencies
+selenium
+ConfigParser
--- scrape.py
+++ scrape.py
@@ -1,3 +1,13 @@
 import configparser
 
 print("YO.")
+
+# Fetch Config
+configparser = configparser.RawConfigParser()   
+configFilePath = r'config.txt'
+configparser.read(configFilePath)
+
+print(configparser.get('General', 'outputPath'))
+print(configparser.get('General', 'subfolderToggle'))
+print(configparser.get('Comic', 'comicName'))
+

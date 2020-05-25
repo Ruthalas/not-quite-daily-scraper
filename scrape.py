fd82dc63b8e6f5284c9935ecfeb621c13e2c9516
Ruthalas <ruthalas.bounce@gmail.com>
2020-05-25 12:09:12 -0700

    Added grabbing basic elements

--- scrape.py
+++ scrape.py
@@ -6,20 +6,29 @@ from selenium import webdriver
 configparser = configparser.RawConfigParser()   
 configFilePath = r'config.txt'
 configparser.read(configFilePath)
-
+# General group
+outputPath = configparser.get('General', 'outputPath')
+subfolderToggle = configparser.get('General', 'subfolderToggle')
+getComments = configparser.get('General', 'getComments')
+# Comic group
 comicName = configparser.get('Comic', 'comicName')
 commentPath = configparser.get('Comic', 'commentPath')
+imageTitlePath = configparser.get('Comic', 'imageTitlePath')
+nextButtonPath = configparser.get('Comic', 'nextButtonPath')
+imagePath = configparser.get('Comic', 'imagePath')
 
 # Set up the browser we'll be using
 driver = webdriver.Firefox()
 # fetch webpage
 driver.get("https://www.truefork.org/Art/comic/cindex.php?44")
 
-# Grab comment
+# Grab content based on the paths provided
 comicComment = driver.find_elements_by_xpath(commentPath)
+comicImage = driver.find_elements_by_xpath(imagePath)
+imageTitle = driver.find_elements_by_xpath(imageTitlePath)
+nextButton = driver.find_elements_by_xpath(nextButtonPath)
 
+print(imageTitle[0].text + "\n" + comicComment[0].get_attribute('innerHTML'))
 
-print(comicComment)
-
-
-
+# Close browser
+driver.close()

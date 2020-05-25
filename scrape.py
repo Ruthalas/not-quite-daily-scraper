db197b984ec37eb9d83baee508da9d1f4c5762c6
Ruthalas <ruthalas.bounce@gmail.com>
2020-05-25 14:03:02 -0700

    Output file naming toggle
    Based on either comic title, or on original image file name

--- scrape.py
+++ scrape.py
@@ -13,6 +13,7 @@ configparser.read(configFilePath)
 outputPath = configparser.get('General', 'outputPath')
 subfolderToggle = configparser.get('General', 'subfolderToggle')
 getComments = configparser.get('General', 'getComments')
+imageNameType = configparser.get('General', 'imageNameType')
 # Comic group
 comicName = configparser.get('Comic', 'comicName')
 commentPath = configparser.get('Comic', 'commentPath')
@@ -41,10 +42,24 @@ imageLocation = comicImage[0].get_attribute('src')
 imageTitleText = imageTitle[0].text
 nextButtonLocation = nextButton[0].get_attribute('href')
 
-imgSavePath = 'pic1'
-path = urlparse(imageLocation).path
-ext = os.path.splitext(path)[1]
-imgSavePathFull = imgSavePath + ext
+# Get the file extension from the URL
+URLpath = urlparse(imageLocation).path
+ext = os.path.splitext(URLpath)[1]
+
+# Get the original image name from the URL
+urlParts = os.path.splitext(URLpath)[0].split("/")
+originalImageName = urlParts[len(urlParts)-1] # (It's the last part when split on '/')
+
+# Name the output image file based on the imageNameType toggle
+if imageNameType == "title":
+    # Set image save name to be the comic title
+    imgSaveName = imageTitleText
+elif imageNameType == "originalFilename":
+    # Set image save name to original image name (based on URL)
+    imgSaveName = originalImageName
+
+# Build the final file path for the image using the output dir, the image name, and the image extension
+imgSavePathFull = os.path.join(outputPath, imgSaveName + ext)
 
 print("Comic Title:   " + imageTitleText + "\nComic Comment: " + comicCommentHTML)
 print("Saving:  " + imageLocation + "\nTo path: " + imgSavePathFull)
